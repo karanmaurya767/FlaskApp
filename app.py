5e3245f0ebfd414313fec42ae1aad2d51ba04b41
Km <your_email@gmail.com>
2026-07-03 11:48:24 +0530

    Connected Railway MySQL

--- app.py
+++ app.py
@@ -101,8 +101,8 @@ def logout():
 
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,4 +1,19 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:00110101@localhost/flask_login"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "mysql+pymysql://root:00110101@localhost/flask_login"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "00110101"
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "00110101")
+
+
+
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:00110101@localhost/flask_login"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = "00110101"
